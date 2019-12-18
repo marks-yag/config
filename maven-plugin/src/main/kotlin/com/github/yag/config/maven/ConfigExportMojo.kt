0e4a27834a5fcb3da3a9e538856d3403ddcf40ec
yag <marks.yag@gmail.com>
2019-12-18 21:45:53 +0800

    Add maven plugin.

---
+++ maven-plugin/src/main/kotlin/com/github/yag/config/maven/ConfigExportMojo.kt
@@ -0,0 +1,43 @@
+package com.github.yag.config.maven
+
+import com.github.yag.config.export
+import org.apache.maven.plugin.AbstractMojo
+import org.apache.maven.plugins.annotations.LifecyclePhase
+import org.apache.maven.plugins.annotations.Mojo
+import org.apache.maven.plugins.annotations.Parameter
+import org.apache.maven.plugins.annotations.ResolutionScope
+import org.apache.maven.project.MavenProject
+import java.io.File
+import java.io.PrintStream
+import java.net.URL
+import java.net.URLClassLoader
+
+@Mojo(name = "export", defaultPhase = LifecyclePhase.PREPARE_PACKAGE, requiresDependencyResolution = ResolutionScope.COMPILE)
+class ConfigExportMojo : AbstractMojo() {
+
+    @Parameter
+    private lateinit var outputPath: String
+
+    @Parameter
+    private lateinit var configClass: String
+
+    @Parameter(property = "project", readonly = true)
+    private lateinit var project: MavenProject
+
+    override fun execute() {
+        val classLoader = URLClassLoader(
+            ArrayList<URL>().apply {
+                addAll(project.testClasspathElements.map {
+                    File(it as String).toURI().toURL()
+                })
+
+            }.toTypedArray(),
+            Thread.currentThread().contextClassLoader
+        )
+
+        File(outputPath).outputStream().use {
+            export(classLoader.loadClass(configClass), PrintStream(it))
+        }
+    }
+
+}
