86a257473f7023de8bd1fde4ec9b5ea5c0bcb568
yag <marks.yag@gmail.com>
2020-10-27 22:44:03 +0800

    Reduce pom.

--- core/src/main/kotlin/com/github/yag/config/Value.kt
+++ core/src/main/kotlin/com/github/yag/config/Value.kt
@@ -1,7 +1,7 @@
 package com.github.yag.config
 
 @Target(AnnotationTarget.FIELD)
-annotation class Value @JvmOverloads constructor(
+annotation class Value (
     val config: String = "",
     val required: Boolean = false,
     val desc: String = ""
